5c5d02da1300303d148bd2721c9f1400c56a76a8
urtesala <urtesaladziute@gmail.com>
2022-10-31 08:59:08 +0300

    init

---
+++ js/sort.js
@@ -0,0 +1,31 @@
+"use strict";
+console.log("sort.js");
+
+const users = [
+  {
+    name: "James",
+    age: 23,
+    gender: "male",
+    town: "London",
+  },
+  {
+    name: "June",
+    age: 25,
+    gender: "female",
+    town: "Paris",
+  },
+  {
+    name: "Bruno",
+    age: 42,
+    gender: "male",
+    town: "Barcelona",
+  },
+  {
+    name: "Carla",
+    age: 28,
+    gender: "female",
+    town: "Prague",
+  },
+];
+
+console.table(users);
