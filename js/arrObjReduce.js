a23b52597cd11e27d1e11584f03ed5d122006360
urtesala <urtesaladziute@gmail.com>
2022-11-03 08:06:45 +0300

    object reduce uzduotys

--- js/arrObjReduce.js
+++ js/arrObjReduce.js
@@ -47,3 +47,81 @@ const ages =
 console.log("ages ===", ages);
 
 // 1gauti visu vardu ilgiu vidurki
+
+const userNameLengthAvg = users.reduce((avg, uObj) => {
+  const currentUserLength = uObj.name.length;
+  const currentUserAvgPart = currentUserLength / users.length;
+  return avg + currentUserAvgPart;
+}, 0);
+console.log(
+  "name lengths",
+  users.map((u) => u.name.length)
+);
+console.log("userNameLengthAvg ===", userNameLengthAvg);
+
+// 2. grazinti jauniausia zmogu
+const youngest = users.reduce((foundYoungest, uObj) => {
+  if (uObj.age < foundYoungest.age) {
+    foundYoungest = uObj;
+  }
+  return foundYoungest;
+}, users[0]);
+
+console.log("youngest ===", youngest);
+
+// kiek nariu yra is londono.
+const londUsersObj = users.reduce(
+  (accObj, uObj) => {
+    // atliekma veiksmus su obj
+    if (uObj.town === "London") {
+      accObj.London++;
+    }
+    // grazinam objekta, ne veiksma su objektu
+    return accObj;
+  },
+  { London: 0 }
+);
+console.log("londUsersObj ===", londUsersObj);
+// const rez = {
+//   London: 2,
+// };
+
+//  gauti toki objekta
+// let gendersObjInit = {
+//   male: [],
+//   female: [],
+// };
+let gendersObjInit = {};
+gendersObjInit["male"] = [];
+gendersObjInit.female = [];
+
+const gendersObj = users.reduce((accObj, uObj) => {
+  if (uObj.gender === "female") {
+    // jei tai moteris
+    accObj.female.push(uObj);
+  } else {
+    // jei tai vyras
+    accObj.male.push(uObj);
+  }
+  // return
+  return accObj;
+}, gendersObjInit);
+
+console.log("gendersObj ===", gendersObj);
+console.log("gendersObjInit ===", gendersObjInit);
+
+// gauti sugrupuuota pagal miestus objekta
+
+const usersByTown = users.reduce((accObj, uObj) => {
+  //  jei toks key(miestas) jau pridetas prie accObj
+  if (accObj[uObj.town]) {
+    // tai as noriu padidinti kieki
+    accObj[uObj.town]++;
+  } else {
+    //  kitu atveju - prideti toki key
+    accObj[uObj.town] = 1;
+  }
+
+  return accObj;
+}, {});
+console.log("usersByTown ===", usersByTown);
